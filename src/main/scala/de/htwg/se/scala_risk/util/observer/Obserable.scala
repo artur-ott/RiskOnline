69b9f63a3b0a9bf0eaa4653da205aac06485f542
Artur Ott <ar431ott@htwg-konstanz.de>
2016-11-28 17:36:40 +0100

    observer implemented

--- src/main/scala/de/htwg/se/scala_risk/util/observer/Obserable.scala
+++ src/main/scala/de/htwg/se/scala_risk/util/observer/Obserable.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.scala_risk.util.observer
 
-class Obserable extends TObservable {
-
+class Obserable {
+  var subscribers:Vector[TObserver] = Vector()
+  def add(s:TObserver) = subscribers=subscribers:+s
+  def remove(s:TObserver) = subscribers=subscribers.filterNot(x => x==s)
+  def notifyObservers = subscribers.foreach(x => x.update())
 }
